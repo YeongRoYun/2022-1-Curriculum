684d962c65edc4dd209660d9a1ba7f9557743b1e
yyr <dbsdudfh01@korea.ac.kr>
2022-03-29 13:20:08 +0900

    .

--- network/socket/src/raw/server.c
+++ network/socket/src/raw/server.c
@@ -5,6 +5,8 @@
 #include<string.h>
 #include<unistd.h>
 #include<stdio.h>
+#include<netinet/tcp.h>
+#include<netinet/ip.h>
 
 #include "header.h"
 
@@ -22,11 +24,11 @@ int main(int argc, char** argv){
     uint16_t client_port = atoi(argv[4]);
     socklen_t addr_len = sizeof(struct sockaddr);
     struct sockaddr_in server_addr, client_addr;
-    struct tcp_header tcph;
-    struct ip_header iph;
+    struct tcphdr tcphdr;
+    struct ip iphdr;
     struct pseudo_header psdh;
-    size_t iph_len = sizeof(struct ip_header);
-    size_t tcph_len = sizeof(struct tcp_header);
+    size_t iph_len = sizeof(struct ip);
+    size_t tcph_len = sizeof(struct tcphdr);
     size_t psdh_len = sizeof(struct pseudo_header);
     uint16_t* tmph;
     uint8_t* buffer;
@@ -63,86 +65,47 @@ int main(int argc, char** argv){
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(client_port);  
     
-    memset(&iph, 0, iph_len);
-    memset(&tcph, 0, tcph_len);
+    memset(&iphdr, 0, iph_len);
+    memset(&tcphdr, 0, tcph_len);
     memset(&psdh, 0, psdh_len);
 
-    iph.version = 4;
-    iph.ihl = 5;
-    iph.tos = 0;
-    iph.totlen = iph_len + tcph_len;
-    iph.id = 0;
-    iph.flags = 2;
-    iph.offset = 0;
-    iph.ttl = 152;
-    iph.protocol = IPPROTO_TCP;
-    iph.source = server_addr.sin_addr.s_addr;
-    iph.dest = client_addr.sin_addr.s_addr;
-    iph.checksum = 0;
-    iph.checksum = checksum((uint16_t*) &iph, iph_len);
+    iphdr.ip_v = 4;
+    iphdr.ip_hl = 5;
+    iphdr.ip_tos = 0;
+    iphdr.ip_len = iph_len + tcph_len;
+    iphdr.ip_id = 0;
+    iphdr.ip_off = 0;
+    iphdr.ip_ttl = 152;
+    iphdr.ip_p = IPPROTO_TCP;
+    iphdr.ip_src = server_addr.sin_addr;
+    iphdr.ip_dst = client_addr.sin_addr;
+    iphdr.ip_sum = checksum((uint16_t*) &iphdr, iph_len);
+
+    tcphdr.th_sport = server_addr.sin_port;
+    tcphdr.th_dport = client_addr.sin_port;
+    tcphdr.th_seq = 0;
+    tcphdr.th_ack = 1;
+    tcphdr.th_off = 5;
+    tcphdr.th_flags = TH_FIN;
+    tcphdr.th_win = htons(512);
+    tcphdr.th_urp = 0;
     
-
-    tcph.source = server_addr.sin_port;
-    tcph.dest = client_addr.sin_port;
-    tcph.sequence = 0;
-    tcph.acknowledge = 1;
-    tcph.offset = 5;
-    tcph.reserved = 0;
-    tcph.urg = 0;
-    tcph.ack = 0;
-    tcph.psh = 0;
-    tcph.rst = 0;
-    tcph.syn = 0;
-    tcph.fin = 1;
-    tcph.window = htons(512);
-
-    // iph.version = (htons(4) >> 12);
-    // iph.ihl = (htons(5) >> 12);
-    // iph.tos = 0;
-    // iph.totlen = htons(iph_len + tcph_len);
-    // iph.id = 0;
-    // iph.flags = (htons(2) >> 13);
-    // iph.offset = 0;
-    // iph.ttl = (htons(152) >> 8);
-    // iph.protocol = (htons(IPPROTO_TCP) >> 8);
-    // iph.source = server_addr.sin_addr.s_addr;
-    // iph.dest = client_addr.sin_addr.s_addr;
-    // iph.checksum = 0;
-    // iph.checksum = checksum((uint16_t*) &iph, iph_len);
-    
-
-    // tcph.source = server_addr.sin_port;
-    // tcph.dest = client_addr.sin_port;
-    // tcph.sequence = 0;
-    // tcph.acknowledge = htonl(1);
-    // tcph.offset = (htons(5) >> 12);
-    // tcph.reserved = 0;
-    // tcph.urg = 0;
-    // tcph.ack = 0;
-    // tcph.psh = 0;
-    // tcph.rst = 0;
-    // tcph.syn = 0;
-    // tcph.fin = 1;
-    // tcph.window = htons(512);
-
-    psdh.source = iph.source;
-    psdh.dest = iph.dest;
+    psdh.source = iphdr.ip_src.s_addr;
+    psdh.dest = iphdr.ip_dst.s_addr;
     psdh.zero = 0;
-    psdh.protocol = iph.protocol;
-    psdh.seglen = tcph.offset*4;
-    tcph.urgent = 0;
+    psdh.protocol = iphdr.ip_p;
+    psdh.seglen = 20;
 
     tmph = (uint16_t*)malloc(2*(psdh_len + tcph_len));
     memcpy(tmph, &psdh, psdh_len);
-    memcpy(tmph+psdh_len, &tcph, tcph_len);
-    tcph.checksum = 0;
-    tcph.checksum = checksum(tmph, psdh_len + tcph_len);
+    memcpy(tmph+psdh_len, &tcphdr, tcph_len);
+    tcphdr.th_sum = checksum(tmph, psdh_len + tcph_len);
     free(tmph);
 
     
     buffer = (uint8_t*)malloc(iph_len + tcph_len);
-    memcpy(buffer, &iph, iph_len);
-    memcpy(buffer+iph_len, &tcph, tcph_len);
+    memcpy(buffer, &iphdr, iph_len);
+    memcpy(buffer+iph_len, &tcphdr, tcph_len);
     
     if(sendto(server_sd, buffer, iph_len+tcph_len, 0, (struct sockaddr*) &client_addr, addr_len) < 0){
         perror("Error: send messages\n");
